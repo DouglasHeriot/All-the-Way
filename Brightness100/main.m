d849c7ac2e1f208bf18bec7220d2f7395e24c5bd
Douglas Heriot <douglas@douglasheriot.com>
2011-12-14 13:33:47 +1100

    Simplified even more.

--- Brightness100/main.m
+++ Brightness100/main.m
@@ -5,12 +5,13 @@
 //  Copyright (c) 2011 Douglas Heriot Design. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <UIKit/UIScreen.h>
 
 int main(int argc, char *argv[])
 {
 	@autoreleasepool
 	{
+		// Yes, that's it.
 		[UIScreen mainScreen].brightness = 1.0;
 		return 0;
 	}
