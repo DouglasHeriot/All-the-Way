ea40f3b4442bf5b68f439529d41133c2ecd21eea
Douglas Heriot <douglas@douglasheriot.com>
2011-12-14 13:02:46 +1100

    Initial Commit

---
+++ Brightness100/DHDBViewController.h
@@ -0,0 +1,13 @@
+//
+//  DHDBViewController.h
+//  Brightness100
+//
+//  Created by Douglas Heriot on 14/12/11.
+//  Copyright (c) 2011 Douglas Heriot Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHDBViewController : UIViewController
+
+@end
---
+++ Brightness100/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Brightness100
+//
+//  Created by Douglas Heriot on 14/12/11.
+//  Copyright (c) 2011 Douglas Heriot Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DHDBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DHDBAppDelegate class]));
+	}
+}
