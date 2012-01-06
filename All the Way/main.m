18b0aeab55da4af07d3b3065a4ba9a7ee18a9abb
Douglas Heriot <douglas@douglasheriot.com>
2012-01-06 15:43:09 +1100

    Added README, and licence

--- All the Way/main.m
+++ All the Way/main.m
@@ -1,8 +1,9 @@
 //
+//  All the Way
 //  main.m
 //
 //  Created by Douglas Heriot on 14/12/11.
-//  Copyright (c) 2011 Douglas Heriot Design. All rights reserved.
+//  Copyright (c) 2011 Douglas Heriot. All rights reserved.
 //
 
 #import <UIKit/UIScreen.h>
@@ -16,3 +17,34 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 }
+
+
+//
+//  Funny obligatory licence, that’s 10 times longer than the actual code:
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//  * Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//  * Neither the name of the <organization> nor the
+//  names of its contributors may be used to endorse or promote products
+//  derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL DOUGLAS HERIOT BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+//  
+//  TL;DR: do what you want, but give me attribution for this 
+//  amazing single line of code I invented, and it's not my fault if anything goes wrong.
+//
---
+++ README.md
@@ -0,0 +1,12 @@
+# [All the Way](http://douglasheriot.com/alltheway/)
+
+##### The actually useful 1-line-of-code iOS app by [Douglas Heriot](http://douglasheriot.com/)
+
+Instantly turns your iOS device brightness up all the way, *without leaving your home screen*. Doesn’t even have any interface or nibs or xibs or even an event loop!
+
+It’s (basically) just **1 line** of code (see [main.m]((https://github.com/DouglasHeriot/All-the-Way/blob/master/All+the+Way/main.m))
+
+    [UIScreen mainScreen].brightness = 1.0;
+
+You may also be interested in [Glare](http://douglasheriot.com/glare/) (actually has a stupid simple interface, and allowed on the App Store).
+
